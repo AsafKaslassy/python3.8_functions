dca709465f4eb584f65c1f2cb2f10224ca39c0a3
AsafKaslassy <44755590+asafkaslassy@users.noreply.github.com>
2020-01-17 17:07:00 +0200

    added regex fix + custom logger
    Signed-off-by: AsafKaslassy <44755590+AsafKaslassy@users.noreply.github.com>

---
+++ utils.py
@@ -0,0 +1,22 @@
+
+
+def sumof(a,b):
+  return a+b
+
+
+def multiplyof(a,b):
+  return a*b
+
+
+def divide(a,b):
+  return a/b
+
+
+def differenceof(a,b):
+  return a-b
+
+
+class MathUtilsException(Exception):
+  pass
+
+
